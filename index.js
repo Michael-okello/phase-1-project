9209bd40f631091eb8ffc3e66bbbb220b36581f2
master <josepzbiz@gmail.com>
2025-06-27 11:15:14 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+//Global variables
+let ideas = [];
+let editingId = null;
+
+//Save/Update idea function
+function saveIdea() {
+    const platform = document.getElementById('platform');
+    const contentType = document.getElementById('contentType');
+    const title = document.getElementById('ideaTitle');
+    const description = document.getElementById('ideaDescription');
+}
+
+
